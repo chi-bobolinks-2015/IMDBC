20087d1e144615dc814037804bbeb9ee1cc0e309
HANA worku & Lauren Markzon <pair+haworku.lmarkzon@devbootcamp.com>
2015-12-07 16:18:32 -0600

    Finished movie MVC

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  # before_action :check_admin, except: [:index, :show]
 
   def index
     @movies = Movie.all
@@ -8,9 +9,45 @@ class MoviesController < ApplicationController
   # def show
   # end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+
+    if @movie.valid?
+      redirect_to movie_path(@movie)
+    else
+      render 'new'
+    end
+  end
+
+  # def edit
+  # end
+  #
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie updated!"
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+
   private
 
   def set_movie
     @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :synopsis, :release_date)
   end
 end
