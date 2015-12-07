7734f80fe213ce1f5f6d6dcc10323f9709add129
HANA worku & Lauren Markzon <pair+haworku.lmarkzon@devbootcamp.com>
2015-12-07 15:35:48 -0600

    Working on movie controller and views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,16 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def index
     @movies = Movie.all
   end
 
-  
+  # def show
+  # end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
 end
