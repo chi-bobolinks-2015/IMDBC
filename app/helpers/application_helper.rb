d45e117e6339c70541fbcee8eadddb726232ba1f
Angela Dyrda & HANA worku <pair+adyrda2.haworku@devbootcamp.com>
2015-12-08 10:23:49 -0600

    add user and critic average rating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
 
-   def critic_reviews(movie)
-    Review.all.where(movie_id: movie.id, :user_id => User.all.where(critic: true))
-  end
-
-  def user_reviews(movie)
-    Review.all.where(movie_id: movie.id, :user_id => User.all.where(critic: false))
-  end
-
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,20 +3,4 @@ class Review < ActiveRecord::Base
   belongs_to :movie
   # has_many :comments
 
-  # def self.critic_ratings(movie)
-  #   Review.all.where(movie_id: movie.id, :user_id => User.all.where(critic: true))
-  # end
-
-  # def self.user_ratings(movie)
-  #   Review.all.where(movie_id: movie.id, :user_id => User.all.where(critic: false)
-  # end
-
-  # def self.critic_reviews(movie)
-  #   Review.all.where(movie_id: movie.id, :user_id => User.all.where(critic: true))
-  # end
-  # #
-  # def self.user_reviews(movie)
-  #   Review.all.where(movie_id: movie.id, :user_id => User.all.where(critic: false))
-  # end
-
 end
