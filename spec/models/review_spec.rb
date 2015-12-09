e3915db80f35d6ef9b795e72facf97197f287a39
David OKeefe <davidpokeefe3@gmail.com>
2015-12-08 20:09:49 -0600

    Update factories to consider critics and begin creating movie specs

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-# describe Review do
-#   let (:review) { FactoryGirl.create(:review) }
+describe Review do
+  let(:review) { FactoryGirl.create(:critic_review) }
 
-#   describe "FactoryGirl setup" do
-#     it "creates a review" do
-#       expect(review).to be_a(Review)
-#     end
-#   end
-# end
+  describe "FactoryGirl setup" do
+    it "creates a review" do
+      expect(review).to be_a(Review)
+    end
+  end
+end
