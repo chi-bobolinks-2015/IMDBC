19e3266ee68f74aedfdd37e9e5c1f0686cca9a39
David O'Keefe <email": null>
2015-12-09 10:33:16 -0600

    Add tests for all movie model methods

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -29,4 +29,37 @@ describe Movie do
       end
     end
   end
+
+  context 'when a critic has made reviews of a movie' do
+    let!(:review) { FactoryGirl.create(:user_review)}
+    let(:movie) { Movie.find(review.movie_id) }
+
+    describe "#critic_reviews" do
+      it 'returns all reviews created by a critic' do
+        expect(movie.user_reviews).to include(review)
+      end
+    end
+  end
+
+  describe "#average_ratingz" do
+    it "returns an average critic rating" do
+      critic1 = FactoryGirl.create(:confirmed_critic)
+      Rate.create(rater: critic1, rateable: movie, rateable_type: "Movie", dimension: "overall", stars: 2)
+      critic2 = FactoryGirl.create(:confirmed_critic)
+      Rate.create(rater: critic2, rateable: movie, rateable_type: "Movie", dimension: "overall", stars: 4)
+
+      expect(movie.average_ratingz(true)).to eq(3)
+    end
+
+    it "rounds ratings to 2 decimal places" do
+      critic1 = FactoryGirl.create(:confirmed_critic)
+      Rate.create(rater: critic1, rateable: movie, rateable_type: "Movie", dimension: "overall", stars: 4)
+      critic2 = FactoryGirl.create(:confirmed_critic)
+      Rate.create(rater: critic2, rateable: movie, rateable_type: "Movie", dimension: "overall", stars: 3)
+      critic3 = FactoryGirl.create(:confirmed_critic)
+      Rate.create(rater: critic3, rateable: movie, rateable_type: "Movie", dimension: "overall", stars: 3)
+
+      expect(movie.average_ratingz(true)).to eq(3.33)
+    end
+  end
 end
